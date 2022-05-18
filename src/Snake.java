9005dd8c5a05f2f2195e8e13ee62be100314443a
Jarrett <jarrettcrump@gmail.com>
2022-05-18 11:24:12 -0500

    snake

--- src/Snake.java
+++ src/Snake.java
@@ -1,26 +1,45 @@
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Snake {
     
     Vector2 pos = new Vector2(2, 1);
     Vector2 vel = new Vector2(1, 0);
-    int length = 0;
-    BodySeg[] bodySegs = new BodySeg[20];
+    int length = 1;
+    ArrayList<BodySeg> bodySegs = new ArrayList<BodySeg>(1);
 
     Snake() {
-        
+        bodySegs.add(new BodySeg(this.pos));
     }
 
-    public void Redraw(Graphics2D g2d) {
-        g2d.setColor(Color.green);
-        g2d.fillRect((int) pos.getX()*(Frame.WIDTH/Frame.STEPSIZE), (int) pos.getY()*(Frame.HEIGHT/Frame.STEPSIZE), Frame.WIDTH/Frame.STEPSIZE, Frame.HEIGHT/Frame.STEPSIZE);
-    }
-
-    public void Move() {
-        pos.add(vel);
-        for (int i = 0; i < bodySegs.length;i++) {
-
+    public void redraw(Graphics2D g2d) {
+        for (int i = 0; i < bodySegs.size();i++) {
+            bodySegs.get(i).redraw(g2d);
         }
+    }
+
+    public void move() {
+        pos.add(vel);
+        bodySegs.get(0).pos = this.pos;
+        for (int i = 1; i < bodySegs.size();i++) {
+            bodySegs.get(i).pos = bodySegs.get(i-1).pos;
+        }
+    }
+
+    public void checkCol(Apple apple) {
+        if (this.pos.equals(apple.pos)) {
+            eat();
+        }
+    }
+
+    public void eat() {
+        length++;
+        System.out.println(length);
+        bodySegs.add(new BodySeg(bodySegs.get(bodySegs.size()-1).pos));
+    }
+
+    public void turn() {
+
     }
 
 }
